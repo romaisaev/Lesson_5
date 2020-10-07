6fa9e9533db53b577d5a2e6f3defd1b4dcaff20e
romaisaev <roma.isaev.001@mail.ru>
2020-10-07 21:05:37 +0300

    Initial Commit

---
+++ L5.5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  L5.5
+//
+//  Created by Рома Исаев on 07.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
